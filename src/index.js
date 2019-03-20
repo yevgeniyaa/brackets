0b1b236ac7caa32a0acc8178d2c2d45a512ba4ac
Yevgeniya.Anossova <Yevgeniya.Anossova@wolterskluwer.com>
2019-03-20 10:45:36 +0600

    implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    bracketsConfig = bracketsConfig.map(config => config.join('')).join('');
+
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        let c = str[i];
+        const pos = bracketsConfig.indexOf(c) !== bracketsConfig.lastIndexOf(c) && stack.includes(c)
+            ? bracketsConfig.lastIndexOf(c)
+            : bracketsConfig.indexOf(c);
+
+        if (pos === -1) {
+            continue;
+        }
+
+        if (pos % 2 === 0) {
+            stack.push(bracketsConfig[pos + 1]);
+        } else if (stack.length === 0 || c !== stack.pop()) {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
